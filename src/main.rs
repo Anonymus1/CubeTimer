df440798bc702ffc15c80cbef0bd4729a2048994
Anonymus1 <AnonymusGithub@gmail.com>
2016-07-13 20:15:40 -0400

    added basic window with red square

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate piston_window;
+extern crate conrod;
+
+use piston_window::*;
+use conrod::{Labelable, Positionable, Sizeable, Colorable, Theme, Ui, Widget};
+//use conrod::color;
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut window: PistonWindow = WindowSettings::new("Cube Timer", [1280, 720])
+        .build().unwrap();
+
+    while let Some(e) = window.next() {
+        window.draw_2d(&e, |c, g| {
+            clear([0.5,0.5,0.5,1.0], g);
+            rectangle([1.0, 0.0, 0.0, 1.0], // red
+                     [0.0, 0.0, 500.0, 500.0], // rectangle
+                     c.transform, g);
+        });
+    }
 }
